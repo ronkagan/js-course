9377a5c2c23214d8fdd195e061ab9fa38f61733d
Ron Kagan <ronkagan@users.noreply.github.com>
2021-07-15 23:59:27 -0400

    Completed lesson on "Values and Variables"

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,39 @@
+/*
+    1. A value is the smallest piece of infromation that a script can contain. These are typically what're logged to the console in examples like:
+        console.log("Jonas");
+        console.log (40 + 8 + 23 - 10);
+        let js = ("amazing");
+            where "amazing" is the value that's assigned to the variable titled "js".
+    2. A variable is stored in a computer's memory after it is "declared."
+        One may think of a variable as a box that is storing a value. The box is labeled. It has a name that stores the thing that is the value, e.g. a first job, for instance, with the title programmer as a string. Note that a box can hold an object. In this case, the value that's held is the object in this example.
+    3. Write vars in camelCase.
+    4. JS vars do not begin with numbers. They can only contain letters, underscores, and the dollar sign. They also cannot be "reserved keywords" in JS like "new" or "function," which throw an unexpected token error.
+    5. Do not start vars with uppercase letters. Those are functions.
+    6. Do not set vars with all uppercase. Those are constants.
+    7. Let the variable name be understandable.
+*/
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Matilda";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let jonas_matilda = "JM";
+let $function = 27;
+
+let person = "jonas";
+let PI = 3.1415;
+
+let myFirstJob = 'Coder';
+let myCurrentJob = 'Teacher';
+
+let job1 = 'programmer';
+let job2 = 'teacher';
+
+console.log(myFirstJob);
