01bc9e7d944501ad65574692848d42ba202d6a69
Ron Kagan <ronkagan@users.noreply.github.com>
2021-07-15 23:35:07 -0400

    initial commit
    Completed 01 Fundamentals Index on linking files (separation of concerns)

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,5 @@
+let js = 'ron';
+if (js === 'katey') alert("I love Katey McCarthy! She is awesome!!! And, she totally rocks at puzzles. Wheeeeeeeeeeeeeee");
+else alert("Ron is learning JavaScript!");
+
+console.log(40 + 8 + 23 - 10);
